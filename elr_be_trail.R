623cf8fcd4653517d73ea173f364decc31c98630
bowmanii <bowmani@uoguelph.ca>
2025-05-03 11:54:16 -0400

    edits

--- elr_be_trail.R
+++ elr_be_trail.R
@@ -3,8 +3,8 @@
 # SiteID: ELR1-R1
 # Author: Isabella Bowman
 # Created: Apr 22 2024
-# Last updated: Apr 22, 2025
-# Description: Barometric Efficiency - ELR1-R1
+# Last updated: May 03, 2025
+# Description: Barometric Efficiency
 
 # https://github.com/bowmanii
 # https://github.com/jkennel
@@ -34,28 +34,38 @@ loc <- read_xlsx("./metadata/transducer_locations.xlsx", na = "NA")
 # create a data.table using the metadata file we just read in
 setDT(loc)
 # redefine DT to only include for ELR1-R1
-loc <- loc[well == "ELR1-R1"]
+#loc <- loc[well == "ELR1-R1"]
 #loc <- loc[well %in% c("ELR1-R1", "ELR1-R2")]
 #loc <- loc[well == "ELR1-R2" | serial == "213655"]
+loc <- loc[well == "ELR2-R1" | serial == "213655"]
 # use grep to only include rsk files from the file_name column
 loc <- loc[grep("rsk", file_name)]
 # list all file names from "data" folder, return full file path, only .rsk files
 fn <- list.files(file_dir, full.names = TRUE, pattern = "*.rsk")
 # cross reference the data files to the data table file_name column
 fn <- fn[basename(fn) %in% loc$file_name]
+
 # grep = matches to an argument 
+
 # subset to only include files from first seal
-fn_sub <- fn[grep("20240626", fn)]
+# use fn_sub for trail transducers as 2 download dates
+# use fn for road transducers as only 1 download date
+
+#fn_sub <- fn[grep("20240626", fn)] # for R1-R1_1
+# return everything except for 20240626
+#fn_sub <- fn[grep("20240626", fn, invert = TRUE)] # for R1-R1_2
+#fn_sub <- fn[grep("20240624|20240626", fn, invert = FALSE)] # for R1-R2_1
+#fn_sub <- fn[grep("20240624|20240626", fn, invert = TRUE)] # for R1-R2_2
 
 # read transducer files using Kennel's package
-baro <- rsk::read_rsk(fn_sub[c(1)],
+baro <- rsk::read_rsk(fn[c(3)],
                     return_data_table = TRUE,
                     include_params = c('file_name'),
                     simplify_names = TRUE,
                     keep_raw = FALSE,
                     raw = TRUE)[variable == "pressure"]
 
-wl <- rsk::read_rsk(fn_sub[c(2)],
+wl <- rsk::read_rsk(fn[c(4)],
                       return_data_table = TRUE,
                       include_params = c('file_name'),
                       simplify_names = TRUE,
@@ -71,7 +81,8 @@ wl_baro <- baro[,.(datetime, baro = value - mean(value))][wl[, .(datetime, value
 
 # subset date range
 # goal = times where there is no pumping, its "stable"
-wl_baro <- wl_baro[between(datetime, as.POSIXct("2024-04-17", tz = "UTC"), as.POSIXct("2024-04-24", tz = "UTC"))]
+wl_baro <- wl_baro[between(datetime, as.POSIXct("2024-04-17", tz = "UTC"), as.POSIXct("2024-04-24", tz = "UTC"))] #R1-R1_1, R1-R2_1
+#wl_baro <- wl_baro[between(datetime, as.POSIXct("2024-07-14", tz = "UTC"), as.POSIXct("2024-07-19", tz = "UTC"))] #R1-R1_2, R1-R2_2
 
 # visualize the data
 # dependent variable = wl pressure
